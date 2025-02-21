ee02ea68ebce864ff4a06b194ed85d10475d5d9d
gabrielaaraujodev <aaraujoslgabriel@gmail.com>
2025-02-21 14:50:35 -0300

    Creating the context class instance and GET request controllers.

---
+++ ApiCatalogo/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> Get()
+    {
+        var products = _context.Products.ToList();
+
+        if(products is null)
+            return NotFound();
+
+        return products;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Product> Get(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+        if (product is null)
+            return NotFound();
+
+        return product;
+    }
+}
