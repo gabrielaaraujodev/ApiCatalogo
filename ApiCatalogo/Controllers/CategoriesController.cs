ee16f9841c0926e7d27be1d2c07f836e9f166942
gabrielaaraujodev <aaraujoslgabriel@gmail.com>
2025-02-23 16:01:04 -0300

    Implementing initial error handling and adding 'No Tracking' to read requests.

--- ApiCatalogo/Controllers/CategoriesController.cs
+++ ApiCatalogo/Controllers/CategoriesController.cs
@@ -19,64 +19,115 @@ public class CategoriesController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Category>> Get()
     {
-        return _context.Categories.ToList();
+        try
+        {
+            return _context.Categories.AsNoTracking().ToList();
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Houve um problema ao acessar a lista de categorias.");
+        }
     }
 
     [HttpGet("{id:int}", Name ="ObterCategoria")]
     public ActionResult<Category> Get(int id)
     {
-        var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
+        try
+        {
+            var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
 
-        if(category == null) 
-            return NotFound();
+            if (category == null)
+                return NotFound("Houve um problema com o id da categoria.");
 
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Houve um problema ao acessar categoria de id = {id}.");
+        }
     }
 
     [HttpGet("products")]
     public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
     {
-        return _context.Categories.Include(p => p.Products).ToList();
+        try
+        {
+            return _context.Categories.Include(p => p.Products).ToList();
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Houve um problema ao acessar a lista de categorias com seus produtos.");
+        }
     }
-
-
-
 
     [HttpPost]
     public ActionResult Post(Category category)
     {
-        if (category is null)
-            return BadRequest();
+        try
+        {
+            if (category is null)
+                return BadRequest("A categoria está incorreta.");
 
-        _context.Categories.Add(category);
-        _context.SaveChanges();
+            _context.Categories.Add(category);
+            _context.SaveChanges();
 
-        return new CreatedAtRouteResult("ObterCategoria", new {id = category.CategoryId}, category);
+            return new CreatedAtRouteResult("ObterCategoria", new { id = category.CategoryId }, category);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Houve um problema adicionar uma nova categoria.");
+        }      
     }
 
     [HttpPut("{id:int}")]
     public ActionResult Put(int id, Category category)
     {
-        if(id != category.CategoryId)
-            return BadRequest();
+        try
+        {
+            if (id != category.CategoryId)
+                return BadRequest("Houve um problema em modificar o id da categoria.");
 
-        _context.Entry(category).State = EntityState.Modified;
-        _context.SaveChanges();
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
 
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Houve um problema modificar uma nova categoria.");
+        }
     }
 
     [HttpDelete("{id:int}")]
     public ActionResult Delete(int id)
     {
-        var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
+        try
+        {
+            var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
 
-        if (category == null)
-            return NotFound();
+            if (category == null)
+                return NotFound("Houve um problema de achar o id da categoria.");
 
-        _context.Categories.Remove(category);
-        _context.SaveChanges();
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
 
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Houve um problema remover esta categoria.");
+        }
     }
 }
--- ApiCatalogo/Controllers/ProductsController.cs
+++ ApiCatalogo/Controllers/ProductsController.cs
@@ -20,60 +20,105 @@ public class ProductsController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Product>> Get()
     {
-        var products = _context.Products.ToList();
+        try
+        {
+            var products = _context.Products.ToList();
 
-        if(products is null)
-            return NotFound();
+            if (products is null)
+                return NotFound("Lista de produtos inexistente.");
 
-        return products;
+            return products;
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Houve um problema ao resgatar a lista de produtos.");
+        }
     }
 
     [HttpGet("{id:int}", Name="ObterProduto")]
     public ActionResult<Product> Get(int id)
     {
-        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+        try
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
 
-        if (product is null)
-            return NotFound();
+            if (product is null)
+                return NotFound($"Produto com o id = {id} inexistente.");
 
-        return product;
+            return product;
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Houve um problema ao resgatar o produto.");
+        }
     }
 
     [HttpPost]
     public ActionResult Post(Product product)
     {
-        if (product is null)
-            return BadRequest();
+        try
+        {
+            if (product is null)
+                return BadRequest("Houve um problema em adicionar o novo produto.");
 
-        _context.Products.Add(product);
-        _context.SaveChanges();
+            _context.Products.Add(product);
+            _context.SaveChanges();
 
-        return new CreatedAtRouteResult("ObterProduto", new { id = product.ProductId, product });
+            return new CreatedAtRouteResult("ObterProduto", new { id = product.ProductId, product });
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Houve um problema em adicionar o produto.");
+        }        
     }
 
     [HttpPut("{id:int}")]
     public ActionResult Put(int id, Product product)
     {
-        if(id != product.ProductId)
-            return BadRequest();
+        try
+        {
+            if (id != product.ProductId)
+                return BadRequest("Houve um problema em alterar o produto.");
 
-        _context.Entry(product).State = EntityState.Modified;
-        _context.SaveChanges();
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
 
-        return Ok(product);
+            return Ok(product);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Houve um problema em alterar o produto.");
+        }   
     }
 
     [HttpDelete("{id:int}")]
     public ActionResult Delete(int id)
     {
-        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+        try
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
 
-        if (product is null)
-            return NotFound();
+            if (product is null)
+                return NotFound("Houve um problema em deleter o produto.");
 
-        _context.Products.Remove(product);
-        _context.SaveChanges();
+            _context.Products.Remove(product);
+            _context.SaveChanges();
 
-        return Ok(product);
+            return Ok(product);
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Houve um problema em remover o produto de id = {id}.");
+        }
     }
 }
